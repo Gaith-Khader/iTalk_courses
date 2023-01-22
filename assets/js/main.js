1daad09c208bd8f9c31639d833af678c94a5f21e
Gaith-Khader <gaith9khader@gmail.com>
2023-01-22 19:41:08 +0200

    NAV & HEADER & LANG

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// nav bar animation
+let navBar=document.getElementById("navBar");
+window.addEventListener("scroll",function(){
+    if(window.scrollY>100){
+        navBar.classList.add("shadow-sm");
+        navBar.classList.replace("bg-transparent","bg-white");
+    }else if(this.window.scrollY<50){
+        navBar.classList.replace("bg-white","bg-transparent");
+        navBar.classList.remove("shadow-sm");
+    }
+    navBar.style.transition='0.5s';
+})
+
+
+
+// slick slider for language section
+$('.slider').slick({
+    dots: false,
+    infinite: false,
+    nextArrow: '<span class="prev_arrow"><i class="fa-solid fa-chevron-right"></i></span>',
+    prevArrow: '<span class="next_arrow"><i class="fa-solid fa-chevron-left"></i></span>',
+    speed: 300,
+    slidesToShow: 7,
+    slidesToScroll: 2,
+    responsive: [
+    {
+    breakpoint: 1024,
+    settings: "unslick"
+    },
+    ]
+});
